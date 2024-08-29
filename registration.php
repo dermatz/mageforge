c3dd03fc8f9b6993ff3a10a3f688eba0c45aafdf
Thomas Hauschild <7961978+Morgy93@users.noreply.github.com>
2024-08-29 15:06:35 +0200

    rename module

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'DerMatz_MageForge',
+    'MageForge_Base',
     __DIR__
 );
